e870f2582528449908a9d6dbfa7a134c9e7609e3
Samarth Shah <57283918+samarth-shah@users.noreply.github.com>
2022-05-21 19:35:44 +0530

    Adding Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_travel_ui/screens/home_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
+} 
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
